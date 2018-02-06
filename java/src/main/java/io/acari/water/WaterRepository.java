97a2684c4e5290bc3b4be817bab1f8d96493eb76
DATBOI <alex@cyclicreference.space>
2018-02-06 14:58:03 -0600

    dependency

--- java/src/main/java/io/acari/water/WaterRepository.java
+++ java/src/main/java/io/acari/water/WaterRepository.java
@@ -1,8 +1,11 @@
 package io.acari.water;
 
 public class WaterRepository {
+  private final WaterSupply waterSupply;
 
-
+  public WaterRepository(WaterSupply waterSupply){
+    this.waterSupply = waterSupply;
+  }
 
   /**
    *

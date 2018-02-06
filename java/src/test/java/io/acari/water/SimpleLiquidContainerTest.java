614285fd9026137d5a9d9d403fc4013531c75ab9
DATBOI <alex@cyclicreference.space>
2018-02-06 15:53:42 -0600

    Some Tests!

---
+++ java/src/test/java/io/acari/water/SimpleLiquidContainerTest.java
@@ -0,0 +1,68 @@
+package io.acari.water;
+
+import org.junit.Test;
+
+/**
+ * Forged in the flames of battle by alex.
+ */
+public class SimpleLiquidContainerTest {
+
+
+  @Test
+  public void constructorShouldThrowIllegalArgumentExceptionWhenGivenNegativeValue() {
+
+  }
+
+  @Test
+  public void fetchTotalCapacityShouldReturnValueConstructedWith() {
+  }
+
+  @Test
+  public void storeWaterShouldStoreReturnMaxCapacityWhenGivenValueEqualToTotalCapacity() {
+
+  }
+
+  @Test
+  public void storeWaterShouldStoreReturnMaxCapacityWhenGivenValueGreaterThanTotalCapacity() {
+
+  }
+
+  @Test
+  public void storeWaterShouldStoreReturnDesiredAmountWhenGivenValueLessThanTotalCapacity() {
+
+  }
+
+  @Test
+  public void storeWaterShouldStoreReturnMaxCapacityWhenGivenValueOneLessThanTotalCapacityTwice() {
+
+  }
+
+  @Test
+  public void storeWaterShouldStoreThrowIllegalArgumentExceptionWhenGivenNegativeOne() {
+
+  }
+
+  @Test
+  public void fetchCurrentVolumeReturnMaxCapacityWhenGivenValueEqualToTotalCapacity() {
+
+  }
+
+  @Test
+  public void fetchCurrentVolumeReturnMaxCapacityWhenGivenValueGreaterThanTotalCapacity() {
+
+  }
+
+  @Test
+  public void fetchCurrentVolumeReturnDesiredAmountWhenGivenValueLessThanTotalCapacity() {
+
+  }
+
+  @Test
+  public void fetchCurrentVolumeReturnMaxCapacityWhenGivenValueOneLessThanTotalCapacityTwice() {
+
+  }
+
+  @Test
+  public void fetchCurrentVolumeShouldReturnZeroWhenNoWaterHasBeenStored() {
+  }
+}

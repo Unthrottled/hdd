d4c771bc87dc077b07d826724fc6373f4c5fe549
DATBOI <alex@cyclicreference.space>
2018-02-13 14:48:16 -0600

    This no worky

--- java/src/main/java/io/acari/water/WaterRepository.java
+++ java/src/main/java/io/acari/water/WaterRepository.java
@@ -14,10 +14,15 @@ public class WaterRepository {
     public LiquidContainer fillContainerHalfWay(LiquidContainer liquidContainer) {
         long halfWay = liquidContainer.fetchTotalCapacity() / 2;
         long l1 = waterSupply.maximumFetchableWater();
-        long l = l1;
-        long trips = (long) Math.ceil(halfWay / (double) l);
+        long l = l1 >= halfWay ? halfWay : l1;
+        long trips =(long) Math.ceil(halfWay / (double) l);
         for (int i = 0; i < trips; i++) {
-
+          long l2 = halfWay - liquidContainer.fetchCurrentVolume();
+          if(l2 < l1){
+            liquidContainer.storeWater(l2);
+          } else {
+            liquidContainer.storeWater(l1);
+          }
         }
         return liquidContainer;
     }

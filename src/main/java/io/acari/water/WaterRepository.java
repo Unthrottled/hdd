ae52b4df35abf28691475176c6f01fc5bcd06f42
DATBOI <alexsimons9999@gmail.com>
2018-02-23 06:40:04 -0600

    Whitespace

--- src/main/java/io/acari/water/WaterRepository.java
+++ src/main/java/io/acari/water/WaterRepository.java
@@ -1,17 +1,17 @@
 package io.acari.water;
 
 public class WaterRepository {
-  private final WaterSupply waterSupply;
+    private final WaterSupply waterSupply;
 
-  public WaterRepository(WaterSupply waterSupply) {
-    this.waterSupply = waterSupply;
-  }
+    public WaterRepository(WaterSupply waterSupply) {
+        this.waterSupply = waterSupply;
+    }
 
-  /**
-   * @param liquidContainer
-   * @return
-   */
-  public LiquidContainer fillContainerHalfWay(LiquidContainer liquidContainer) {
-    return liquidContainer;
-  }
+    /**
+     * @param liquidContainer
+     * @return
+     */
+    public LiquidContainer fillContainerHalfWay(LiquidContainer liquidContainer) {
+        return liquidContainer;
+    }
 }

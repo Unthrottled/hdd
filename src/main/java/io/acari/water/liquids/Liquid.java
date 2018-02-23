a823f99eb41c6e89e2d141eda23c578ad9587ee4
DATBOI <alexsimons9999@gmail.com>
2018-02-23 06:23:53 -0600

    Isn't it nice to have just to worry about numbers inside of a class and not everywhere

--- src/main/java/io/acari/water/liquids/Liquid.java
+++ src/main/java/io/acari/water/liquids/Liquid.java
@@ -3,6 +3,10 @@ package io.acari.water.liquids;
 public class Liquid {
     private final long amount;
 
+    /**
+     * @param amount any number above -1
+     * @throws IllegalArgumentException if given any number below zero
+     */
     Liquid(long amount) {
         this.amount = amount;
     }

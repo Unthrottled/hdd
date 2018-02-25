a18e273b866b84875f2057f2a967a7c672709716
DATBOI <alexsimons9999@gmail.com>
2018-02-24 20:41:27 -0600

    more worky tests

--- src/test/java/io/acari/water/WaterRepositoryTest.java
+++ src/test/java/io/acari/water/WaterRepositoryTest.java
@@ -1,53 +1,63 @@
 package io.acari.water;
 
+import io.acari.water.liquids.Water;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class WaterRepositoryTest {
 
 
-  @Test
-  public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFullWheSupplyHasLessThanAsked() {
-    WaterSupply waterSupply = Mockito.mock(WaterSupply.class);
-    Mockito.when(waterSupply.fetchWater(250L)).thenReturn(20L);
-     WaterRepository testSubject = new WaterRepository(waterSupply);
-    LiquidContainer simpleLiquidContainer = new SimpleLiquidContainer(500);
-    LiquidContainer result = testSubject.fillContainerHalfWay(simpleLiquidContainer);
-    assertEquals(250, result.fetchCurrentVolume());
-  }
+    @Test
+    public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFullWheSupplyHasLessThanAsked() {
+        WaterSupply waterSupply = Mockito.mock(WaterSupply.class);
+        Mockito.when(waterSupply.fetchWater(250L)).thenReturn(new Water(250L));
+        WaterRepository testSubject = new WaterRepository(waterSupply);
+        LiquidContainer simpleLiquidContainer = new SimpleLiquidContainer(500);
+        LiquidContainer result = testSubject.fillContainerHalfWay(simpleLiquidContainer);
+        assertTrue(result.fetchCurrentVolume()
+                .map(new Water(250)::equals)
+                .orElse(false));
+    }
 
 
-  @Test
-  public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFullWheSupplyHasEnough() {
-    WaterSupply waterSupply = Mockito.mock(WaterSupply.class);
-    Mockito.when(waterSupply.fetchWater(250L)).thenReturn(250L);
-    WaterRepository testSubject = new WaterRepository(waterSupply);
-    LiquidContainer simpleLiquidContainer = new SimpleLiquidContainer(500);
-    LiquidContainer result = testSubject.fillContainerHalfWay(simpleLiquidContainer);
-    assertEquals(250, result.fetchCurrentVolume());
-  }
+    @Test
+    public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFullWheSupplyHasEnough() {
+        WaterSupply waterSupply = Mockito.mock(WaterSupply.class);
+        Mockito.when(waterSupply.fetchWater(250L)).thenReturn(new Water(250L));
+        WaterRepository testSubject = new WaterRepository(waterSupply);
+        LiquidContainer simpleLiquidContainer = new SimpleLiquidContainer(500);
+        LiquidContainer result = testSubject.fillContainerHalfWay(simpleLiquidContainer);
+        assertTrue(result.fetchCurrentVolume()
+                .map(new Water(250)::equals)
+                .orElse(false));
+    }
 
 
-  @Test
-  public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFullWheSupplyHasLessThanAsked_II() {
-    WaterSupply waterSupply = Mockito.mock(WaterSupply.class);
-    Mockito.when(waterSupply.fetchWater(500L)).thenReturn(50L);
-   WaterRepository testSubject = new WaterRepository(waterSupply);
-    LiquidContainer simpleLiquidContainer = new SimpleLiquidContainer(1000);
-    LiquidContainer result = testSubject.fillContainerHalfWay(simpleLiquidContainer);
-    assertEquals(500, result.fetchCurrentVolume());
-  }
+    @Test
+    public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFullWheSupplyHasLessThanAsked_II() {
+        WaterSupply waterSupply = Mockito.mock(WaterSupply.class);
+        Mockito.when(waterSupply.fetchWater(500L)).thenReturn(new Water(500L));
+        WaterRepository testSubject = new WaterRepository(waterSupply);
+        LiquidContainer simpleLiquidContainer = new SimpleLiquidContainer(1000);
+        LiquidContainer result = testSubject.fillContainerHalfWay(simpleLiquidContainer);
+        assertTrue(result.fetchCurrentVolume()
+                .map(new Water(500)::equals)
+                .orElse(false));
+    }
 
 
-  @Test
-  public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFullWheSupplyHasEnough_II() {
-    WaterSupply waterSupply = Mockito.mock(WaterSupply.class);
-    Mockito.when(waterSupply.fetchWater(500L)).thenReturn(500L);
-    WaterRepository testSubject = new WaterRepository(waterSupply);
-    LiquidContainer simpleLiquidContainer = new SimpleLiquidContainer(1000);
-    LiquidContainer result = testSubject.fillContainerHalfWay(simpleLiquidContainer);
-    assertEquals(500, result.fetchCurrentVolume());
-  }
+    @Test
+    public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFullWheSupplyHasEnough_II() {
+        WaterSupply waterSupply = Mockito.mock(WaterSupply.class);
+        Mockito.when(waterSupply.fetchWater(500L)).thenReturn(new Water(500L));
+        WaterRepository testSubject = new WaterRepository(waterSupply);
+        LiquidContainer simpleLiquidContainer = new SimpleLiquidContainer(1000);
+        LiquidContainer result = testSubject.fillContainerHalfWay(simpleLiquidContainer);
+        assertTrue(result.fetchCurrentVolume()
+                .map(new Water(500)::equals)
+                .orElse(false));
+    }
 }

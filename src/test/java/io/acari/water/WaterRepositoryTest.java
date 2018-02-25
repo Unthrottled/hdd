5a1da42655b03c710e45be4b97c042b6c44bc816
DATBOI <alexsimons9999@gmail.com>
2018-02-25 13:41:10 -0600

    Testsgit add .

--- src/test/java/io/acari/water/WaterRepositoryTest.java
+++ src/test/java/io/acari/water/WaterRepositoryTest.java
@@ -14,7 +14,7 @@ public class WaterRepositoryTest {
 
 
     @Test
-    public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFullWheSupplyHasLessThanAsked() {
+    public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFull() {
         WaterSupply waterSupply = Mockito.mock(WaterSupply.class);
         Mockito.when(waterSupply.fetchWater(250L)).thenReturn(new Water(250L));
         WaterRepository testSubject = new WaterRepository(waterSupply);
@@ -27,7 +27,7 @@ public class WaterRepositoryTest {
 
 
     @Test
-    public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFullWheSupplyHasLessThanAsked_II() {
+    public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFull_II() {
         WaterSupply waterSupply = Mockito.mock(WaterSupply.class);
         Mockito.when(waterSupply.fetchWater(500L)).thenReturn(new Water(500L));
         WaterRepository testSubject = new WaterRepository(waterSupply);
@@ -35,6 +35,19 @@ public class WaterRepositoryTest {
         LiquidContainer result = testSubject.fillContainerHalfWay(simpleLiquidContainer);
         assertTrue(result.fetchCurrentVolume()
                 .map(new Water(500)::equals)
+                .orElse(false));
+    }
+
+
+    @Test
+    public void fillContainerHalfWayShouldReturnAContainerThatIsHalfFullAndHalfNeedsToBeFloatingPoint() {
+        WaterSupply waterSupply = Mockito.mock(WaterSupply.class);
+        Mockito.when(waterSupply.fetchWater(131L)).thenReturn(new Water(131L));
+        WaterRepository testSubject = new WaterRepository(waterSupply);
+        LiquidContainer simpleLiquidContainer = new SimpleLiquidContainer(263);
+        LiquidContainer result = testSubject.fillContainerHalfWay(simpleLiquidContainer);
+        assertTrue(result.fetchCurrentVolume()
+                .map(new Water(131)::equals)
                 .orElse(false));
     }
 }

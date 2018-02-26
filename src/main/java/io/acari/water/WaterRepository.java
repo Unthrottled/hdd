e9d3aca1d5df4894316ec3d2bff0253ce0f6c23f
DATBOI <alexsimons9999@gmail.com>
2018-02-26 06:23:36 -0600

    Updated behaviour

--- src/main/java/io/acari/water/WaterRepository.java
+++ src/main/java/io/acari/water/WaterRepository.java
@@ -11,8 +11,12 @@ public class WaterRepository {
     }
 
     /**
-     * @param liquidContainer
-     * @return
+     * Fills the given container to the floor of the halfway mark.
+     *
+     * @param liquidContainer a container that is less than half full.
+     * @return a container that is half full.
+     * @throws IllegalArgumentException when given a more than half filled container
+     *                                  (we are half full people here)
      */
     public LiquidContainer fillContainerHalfWay(LiquidContainer liquidContainer) {
         return liquidContainer;

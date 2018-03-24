9217916855289505342eb3c3b0e0da502ddefc8e
DATBOI <alexsimons9999@gmail.com>
2018-03-24 16:02:39 -0500

    Liquid tests pass!

--- src/main/java/io/acari/water/liquids/Liquid.java
+++ src/main/java/io/acari/water/liquids/Liquid.java
@@ -1,5 +1,8 @@
 package io.acari.water.liquids;
 
+import java.util.Objects;
+import java.util.Optional;
+
 public class Liquid {
     private final long amount;
 
@@ -8,10 +11,25 @@ public class Liquid {
      * @throws IllegalArgumentException if given any number below zero
      */
     Liquid(long amount) {
-        this.amount = amount;
+        this.amount = Optional.of(amount)
+        .filter(aLong -> aLong > -1)
+        .orElseThrow(()->new IllegalArgumentException("Cannot create liquid instance with value " + amount));
     }
 
     public long getAmount() {
         return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Liquid)) return false;
+        Liquid liquid = (Liquid) o;
+        return amount == liquid.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }

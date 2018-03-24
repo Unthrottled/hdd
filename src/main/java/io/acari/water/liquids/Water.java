ac260efe977f4149d802f4694e67134009eb727a
DATBOI <alexsimons9999@gmail.com>
2018-03-24 16:03:59 -0500

    Water tests pass!

--- src/main/java/io/acari/water/liquids/Water.java
+++ src/main/java/io/acari/water/liquids/Water.java
@@ -5,4 +5,9 @@ public class Water extends Liquid {
     public Water(long amount) {
         super(amount);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Water && super.equals(o);
+    }
 }
